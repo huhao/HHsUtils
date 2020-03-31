c5cd031e20bf53e3ab75576bfc3eb8881bce2854
HH君 <hhlovedlh@gmail.com>
2020-03-31 23:50:54 +0800

    up

--- src/main/java/pers/huhao/HHsUtils/utils/HHsHttpUtil.java
+++ src/main/java/pers/huhao/HHsUtils/utils/HHsHttpUtil.java
@@ -2,6 +2,7 @@ package pers.huhao.HHsUtils.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class HHsHttpUtil {
 
@@ -9,6 +10,22 @@ public class HHsHttpUtil {
 
     public static Map<String, Object> getUrlParams(String param) {
         Map<String, Object> map = new HashMap<>();
+
+        if (HHsStringUtil.isBlank(param)) return map;
+
+        String[] params = param.split("&");
+
+        for (int i = 0; i < params.length; i++) {
+            String[] p = params[i].split("=");
+            if (p.length == 2) {
+                map.put(p[0], p[1]);
+            }
+        }
+        return map;
+    }
+
+    public static Map<String, Object> getSortedUrlParams(String param) {
+        Map<String, Object> map = new TreeMap<>();
 
         if (HHsStringUtil.isBlank(param)) return map;
 
